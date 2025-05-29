a7d1bcfe89a9e9530118f79750ee2f1ad7e93e55
wesleyg97322 <wesley@lac-coloc.fr>
2025-05-29 21:10:09 +0200

    backend(add): TMDB movie schema

---
+++ backend/schemas/movie.py
@@ -0,0 +1,27 @@
+from core.config import get_config
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+class Movie(BaseModel):
+    adult: bool = True
+    backdrop_path: Optional[str] = None
+    genre_ids: List[int] = None
+    id: int = 0
+    original_language: str = ""
+    original_title: str = ""
+    overview: str = ""
+    popularity: float = 0.0
+    poster_path: Optional[str] = None
+    release_date: str = ""
+    title: str = ""
+    video: bool = True
+    vote_average: float = 0.0
+    vote_count: int = 0
+
+
+class TMDBMovie(BaseModel):
+    page: int = 0
+    results: List[Movie] = None
+    total_pages: int = 0
+    total_results: int = 0
