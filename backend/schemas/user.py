58a04460ea03f8378b8b32b65e716acc9f3a814e
wesleyg97322 <wesley@lac-coloc.fr>
2025-06-01 17:27:23 +0200

    update(feat): create user method

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,9 +1,24 @@
-from models.user import user as userModel
+from typing import Self
+from uuid import uuid4
 
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     id: str
-    number: int
+    index: int
+    session_id: str
     name: str = ""
+
+    @classmethod
+    def create_user(cls, index: int, session_id: str) -> Self:
+        """
+        Create a new user without name
+        """
+        user = User(**{
+            "id": uuid4().hex,
+            "index": index,
+            "session_id": session_id
+        })
+
+        return user
